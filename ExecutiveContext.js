191916bbd1ad48c387e6f52710f38f5db078e727
Km-reshma <reshmak0811@gmail.com>
2026-06-24 11:56:07 +0530

    excutionconext

---
+++ ExecutiveContext.js
@@ -0,0 +1,20 @@
+/*var n=2;
+function square (num){
+    var ans = num*num;
+    return ans ;
+}
+var square2 =  square(n);
+var square4 =  square(4);*/
+
+/*var x =7;
+
+function getName(){
+    console.log("namaste javascripts");
+}
+
+getName(); //here we call the fiction show the output "namaste javascript"
+console.log(x); //print the output to the console value of x
+console.log(getName); */ // we does not invoke the function rather we print the function name so we get the whole function block as similar to the code 
+
+console.log("hello");
+document.write("Hi Priya");
