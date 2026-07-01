af0168f151d5761b6f911474256b8aa9b09e71d0
Km-reshma <reshmak0811@gmail.com>
2026-07-01 19:44:14 +0530

    debugger in loop

--- keywords/keywords.js
+++ keywords/keywords.js
@@ -13,12 +13,26 @@ console.log(x);
 
 // run at browser by using "debugger " keyword
 
+/*
 let a=10;              
 console.log(a);
 debugger;         //debuger through the coding(),  means debugging by using "debugger"keyword
 
 var x =10;
-console.log(x);
+console.log(x);  //jaha breakpoints hai debugger ke waha par debugger pause(halt) hoga
 debugger;
+*/
+
+//............
+
+let i=1;
+do{
+
+    console.log(i);
+    debugger;     //loop mein likha hai debugger , the debugger bar bar exexcute hoga or jb jb debugeer cgalega to halt hoga like 1sr loop ...1 and then 2ndt lopp ...print print 2.....and so on
+    i++;
+}while(i<=5);
+
+
 
 
