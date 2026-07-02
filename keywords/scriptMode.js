e7fbcf5444f1292f7403632eca7bfacaf0083bea
Km-reshma <reshmak0811@gmail.com>
2026-07-02 19:08:51 +0530

    function script mode

---
+++ keywords/scriptMode.js
@@ -0,0 +1,54 @@
+// without strict mode
+
+
+//i =0;
+//console.log(i)  // this is oldest way in the js
+                // if you written this code (without keyword ka variable) at under any funct then ye variable global variable ban jata hai 
+                // no error print "0
+
+                
+
+    // ############    with Script mode - using string literals
+ 
+//"use strict"
+
+//i=0;
+//console.log(i);  // gives error now ......i=0;....ReferenceError: i is not defined
+                 // In Strict Mode, you dont create the variable without keyword , must be use any defined keyword(let, var, const)
+
+//let i=0;
+//console.log(i); // now no error  bcz ysing string literals "use strict"
+
+/*
+"use strict"
+
+let i=0;
+console.log(i);
+
+function f1(){
+
+    a=0;
+    console.log(a);
+}
+
+f1();                // show the error due to strict mode is applicable whole code   ....ReferenceError: a is not defined 
+ */
+
+
+// struct mode application only function..............................
+
+
+i=0;
+console.log(i);    // print "0" no error bcz strict mode is not applicable at globally
+
+
+function f2(){
+
+    "use strict"
+
+     let a=0;
+     console.log(a);  // print "0" no error bcz strict mode is  applicable at only one only function f2().
+}
+
+f2();
+
