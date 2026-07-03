bfb38951d5645df648a090a71b474e032db51e35
Km-reshma <reshmak0811@gmail.com>
2026-07-03 16:09:19 +0530

    addition modulus and divide operators

--- Operators/operators.js
+++ Operators/operators.js
@@ -1,1 +1,31 @@
+ let a;
+ a = 3+4;
+ console.log(a); // output =7
+
+let b;
+ b = 3/4;
+ console.log(b);  // output is 0.75 but in c and c++ languages gives 0 bcz they gives the result as quotient (if divinded < divisor = 0 in c)
+
+
+ let c;
+ c = 3/4;
+ console.log(Math.floor(c)); // if we want only quotient in javascript so we use "Math.floor()" function
+
+let d;
+ d = 10%4;
+ console.log(d);  // gives the output as the remainder " output is 2"
+                  // perfect divisible =remainder is 0
+
+
+let d;
+ d = -10%4;
+ console.log(d);  // out put is -2
+
+
+let d;
+ d = 10%-4;
+ console.log(d);  // output is 2 bcz sign is seen at numerator is positive give positive result ans if neg then neg result
+
  
+
+
