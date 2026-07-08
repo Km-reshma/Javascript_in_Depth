eef1844f11491f3eeea9bf6e47f21562117af002
Km-reshma <reshmak0811@gmail.com>
2026-07-08 11:45:42 +0530

    logical operator

---
+++ Operators/logical.js
@@ -0,0 +1,22 @@
+        //  ########Logical Operators   ###################
+/*
+1. && (Logical AND) Operator:
+The && operator returns true if both operands are true;
+
+2. || (Logical OR) Operator:
+The || operator returns true if at least one of the operands is true;
+
+3. ! (Logical NOT) Operator:
+The ! operator returns true if the operand is false, and false if the operand is true.
+
+
+
+let a=5;
+let b=!a;
+console.log(a,b);    // out put : 5 false because a is 5 which is true and !a will return false
+                     // a is non zero number so it is true and !a will return false bcz a is true and !a is false
+ */
+                     
+let a=0;
+let b=!a;
+console.log(a,b); 
