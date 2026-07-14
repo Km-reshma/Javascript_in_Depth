645b6eb0e4467b15d7bbdee53efbceaf6b5b67b7
Km-reshma <reshmak0811@gmail.com>
2026-07-14 16:06:29 +0530

    Function syntax

---
+++ FunEventCallAsynchronousJS/function.js
@@ -0,0 +1,24 @@
+// _________________####### Functions #########_______________________________________________________
+/*
+A function is a reusable block of code that performs a specific task.
+It can take parameters (inputs)and process them and can return a value.
+A function is executed when “something” invokes it (calls it). It is a reusable block of code.
+Functions allow you to encapsulate code that performs a specific task into a block that you can call multiple times.
+
+" Functions are “first-class objects” in JavaScript, which means they can be properties of objects, elements of arrays, arguments to other functions, and more. "
+
+Functions help in organizing code, improving reusability, and making programs modular.
+
+Syntax of function:-
+
+//keyword  function_name
+    |    |      _____________Parameters
+    |    |     |                              */
+function sum (x,y){
+
+    return x+y;               // function body andy return keyword
+}
+
+let result = sum(4,5);        // (4,5) as an arguement
+  //            |_____________  function call
+console.log(result);
