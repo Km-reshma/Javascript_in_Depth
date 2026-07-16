48399d6b2ff84bd26352339fff3b16f1e3955163
Km-reshma <reshmak0811@gmail.com>
2026-07-16 10:44:11 +0530

    call backworking of coe

--- FunEventCallAsynchronousJS/callback.js
+++ FunEventCallAsynchronousJS/callback.js
@@ -21,3 +21,132 @@ function f2(callback){
 f2(function () {                 // anonymous function ()
     console.log("Hello");
 });
+
+
+
+
+//____________Explanation how does it works_______________________________________________________
+
+
+//Example-------------------
+
+function f1() {
+    console.log("Hello");
+}
+
+function f2(callback) {
+    console.log("Inside f2");
+
+    callback();
+}
+
+f2(f1);
+
+
+Step 1: Functions Memory me store hote hain____________________________________________________
+
+JavaScript sabse pehle dono functions ko memory me store karta hai.
+
+Memory-
+
+f1  ─────► Function
+f2  ─────► Function
+
+Abhi kuch execute nahi hua.
+
+
+
+Step 2: Function call__________________________________________________________________________________________
+
+f2(f1);
+
+JavaScript dekhta hai:
+
+f2(
+   f1
+)
+
+remember:-------
+
+Yahan  f1  likha hai,  f1()  nahi.
+
+Matlab:---------
+
+"Ye function le lo."
+
+JavaScript internally aisa sochta hai:              callback = f1;
+
+Memory:------------
+
+callback
+    │
+    ▼
+   f1 Function
+
+
+Step 3: f2 ke andar aaye________________________________________________________________________
+
+function f2(callback) {
+
+    console.log("Inside f2");
+
+    callback();
+
+}
+
+Pehle ye line chalegi:    bcz for this f2 (f1)
+
+console.log("Inside f2");
+
+Output:  Inside f2
+
+
+Step 4: callback()____________________________________________________________________________
+
+Ab JavaScript dekhta hai:
+
+callback();
+
+Usko pata hai:            callback = f1
+
+To ye line automatically ban jati hai:            f1();
+
+
+Step 5: f1 execute________________________________________________________________________________________
+
+Ab f1() ke andar jata hai.
+
+function f1() {
+    console.log("Hello");
+}
+
+Output:   Hello
+
+Final Output----------
+
+Inside f2
+Hello
+
+Flow chart-
+
+      f2(f1)
+
+        │
+        ▼
+    callback = f1
+
+        │
+        ▼
+     Inside f2
+
+        │
+        ▼
+     callback()
+
+        │
+        ▼
+      f1()
+
+        │
+        ▼
+      Hello
