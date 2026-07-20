0b1d55a389a5fb93de4f3ce605c1744ae9fe3e78
Km-reshma <reshmak0811@gmail.com>
2026-07-20 16:50:00 +0530

    dom API

--- Dom&DomAPI/Dom.js
+++ Dom&DomAPI/Dom.js
@@ -3,6 +3,8 @@
 /*
 DOM --> stands for Document Object Model.
 
+It is the map of HTML page element(object).
+
 It acts as a bridge between HTML and JavaScript, allowing JavaScript to read, modify, create, and delete HTML elements dynamically.
 
 When a browser loads an HTML page, it doesn't directly work with the HTML text. Instead, it converts the HTML into a tree-like structure called the DOM Tree.
@@ -56,6 +58,25 @@ DOM API-------------------------------------------------------------------------
 
 The DOM API is the set of interfaces, properties, and methods provided by the browser to interact with the DOM tree.
 
+DOM API browser ke dwara provide ki gayi built-in JavaScript functions aur objects ka collection hai. In functions ko hum apne JavaScript program se call karte hain, aur inke through browser ke C++ me implement kiye gaye DOM engine ko access aur manipulate karte hain.
+
+DOM API browser dwara provide ki gayi built-in methods aur properties ka collection hai. In methods ko JavaScript se call karke browser ke C++ me implement DOM tree ko access aur manipulate kiya jata hai.
+
+DOM API JavaScript ka part nahi hai. Ye Web APIs ka hissa hai, jo browser provide karta hai. JavaScript sirf in APIs ko use (call) karti hai.
+
+
+JavaScript Code
+       │
+       ▼
+DOM API (document, Element, Node)
+       │
+       ▼
+Browser Engine (C++ Implementation)
+       │
+       ▼
+HTML DOM Tree
+
+
 Main DOM Interfaces----------------------------------------------------------------------------
 
 Document — Represents the whole page.
@@ -64,6 +85,8 @@ Node — Base interface for all DOM nodes (elements, text, comments).
 NodeList / HTMLCollection — Array-like collections of nodes.
 EventTarget — Allows attaching event listeners.
 */
+
+
 
 
 
