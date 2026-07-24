f4fe3faa63e63a413d5a062b2bf56dcb0aa1375c
Km-reshma <reshmak0811@gmail.com>
2026-07-24 19:21:40 +0530

    stores multiple datatypes

--- Array/array.js
+++ Array/array.js
@@ -2,10 +2,21 @@
 
 let arr = [1, 2, 3];
 console.log(arr, "and length of the array is :" +arr.length);       //[ 1, 2, 3 ] and length of the array is :3
-[ 1, 2, 3 ] and length of the array is :3
 
 arr.push(4);                                                        // Array ka size automatically badh gaya
 console.log(arr, "and length of the array is :" +arr.length);       //[ 1, 2, 3, 4 ] and length of the array is :4
 
 
-//
+// store maultiple data types in a single variable 
+
+let data = [10, "Hello", true, {name: "John"}, [1, 2]];
+console.log(data);                                                  // [ 10, 'Hello', true, { name: 'John' }, [ 1, 2 ] ]
+
+
+
+// Array is a apecial type of object 
+
+let arr1 = [10, 20];
+console.log(typeof arr1);                                           // object
+
+console.log(Array.isArray(arr1));                                   // true
