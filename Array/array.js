ceab81792c79422d2325309a14f1d8cdb894941b
Km-reshma <reshmak0811@gmail.com>
2026-07-24 19:15:28 +0530

    dyanamically size increased or decreased of the array

---
+++ Array/array.js
@@ -0,0 +1,11 @@
+// dyanamically size increased or decreased
+
+let arr = [1, 2, 3];
+console.log(arr, "and length of the array is :" +arr.length);       //[ 1, 2, 3 ] and length of the array is :3
+[ 1, 2, 3 ] and length of the array is :3
+
+arr.push(4);                                                        // Array ka size automatically badh gaya
+console.log(arr, "and length of the array is :" +arr.length);       //[ 1, 2, 3, 4 ] and length of the array is :4
+
+
+//
