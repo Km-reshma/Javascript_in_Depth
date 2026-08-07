937859f088ec97b499ed1b2ee8966a6bbcca5ef2
Km-reshma <reshmak0811@gmail.com>
2026-08-07 14:21:53 +0530

    concat()

--- String/strMethod.js
+++ String/strMethod.js
@@ -1,4 +1,4 @@
-
+/*
 //______________________-########## length Property ################ _______________________________________
 
 //length = Returns the number of characters.
@@ -29,3 +29,19 @@ console.log(str2.charCodeAt(0));                           //65
 console.log(str2.charCodeAt(3));                           //67
 console.log(str2.charCodeAt(4));                           //69
 console.log(str2.charCodeAt(5));                           //97
+*/
+
+
+
+
+//____________________########### concat() method ####################___________________________________
+
+//concat() is used to join (combine) arrays or strings without changing the original one.
+
+let str1 ="Hello";
+let str2 = "Alice";
+
+let str = str1.concat(str2);
+console.log(str);                       //HelloAlice, if we write " let str = str1.concat(" ",str2); "
+
+console.log(str1.concat(" ",str2));      //Hello Alice                     if we add the empty string then gives the 1 space 
